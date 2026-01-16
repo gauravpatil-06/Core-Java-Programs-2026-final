be84f8c6681b6a46201212fd05b1d91cca975ef1
Gaurav Patil <gp949958@gmail.com>
2026-01-16 20:43:25 +0530

    added more while programs

---
+++ ControlStatement/src/com/gaurav/loopingstatement/whileloop/NumberSeries.java
@@ -0,0 +1,26 @@
+package com.gaurav.loopingstatement.whileloop;
+
+public class NumberSeries 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Number Series Sequential\n");
+		
+		int i=1;
+		while(i <= 10)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+		System.out.println("\nNumber Series Reverse\n");
+		
+		int j=10;
+		while(j >= 1)
+		{
+			System.out.println(j);
+			j--;
+		}
+		
+	}
+}
