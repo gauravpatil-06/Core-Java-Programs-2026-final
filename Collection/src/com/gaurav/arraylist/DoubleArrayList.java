4ece9580c5f72a20c286bf7617abcf4b382c7c77
Gaurav Patil <gp949958@gmail.com>
2026-03-01 14:45:12 +0530

    added more arraylist types

---
+++ Collection/src/com/gaurav/arraylist/DoubleArrayList.java
@@ -0,0 +1,24 @@
+package com.gaurav.arraylist;
+
+import java.util.ArrayList;
+
+public class DoubleArrayList 
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<Double> al = new ArrayList<Double>();
+		
+		al.add(100.50);
+		al.add(200.50);
+		al.add(300.50);
+		al.add(400.50);
+		al.add(500.50);
+		al.add(600.50);
+		al.add(700.50);
+		al.add(800.50);
+		al.add(900.50);
+		al.add(1000.50);
+		
+		System.out.println(al);
+	}
+}
---
+++ Collection/src/com/gaurav/arraylist/FloatArrayList.java
@@ -0,0 +1,24 @@
+package com.gaurav.arraylist;
+
+import java.util.ArrayList;
+
+public class FloatArrayList 
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<Float> al = new ArrayList<Float>();
+
+        al.add(10.5f);
+        al.add(20.5f);
+        al.add(30.5f);
+        al.add(40.5f);
+        al.add(50.5f);
+        al.add(60.5f);
+        al.add(70.5f);
+        al.add(80.5f);
+        al.add(90.5f);
+        al.add(100.5f);
+        
+        System.out.println(al);
+	}
+}
