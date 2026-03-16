c09c95a8efe3d33f3e01e9cc0ebb2f57c77692ad
Gaurav Patil <gp949958@gmail.com>
2026-03-16 17:00:00 +0530

    added student and user linkedlist

---
+++ Collection/src/com/gaurav/linkedlist/UserLinkedList.java
@@ -0,0 +1,25 @@
+package com.gaurav.linkedlist;
+
+import java.util.LinkedList;
+
+public class UserLinkedList
+{
+	public static void main(String[] args)
+	{
+		LinkedList<User> user = new LinkedList<User>();
+
+		user.add(new User(101, "gaurav", "gaurav@gmail.com", "pass123", 9876543210L));
+		user.add(new User(102, "vinod", "vinod@yahoo.com", "pass456", 8765432109L));
+		user.add(new User(103, "akash", "akash@gmail.com", "pass789", 9123456789L));
+		user.add(new User(104, "suraj", "suraj@gmail.com", "pass111", 8899776655L));
+		user.add(new User(105, "abhishek", "abhi@gmail.com", "pass222", 9988776655L));
+
+		for (User u : user)
+		{
+			if (u.getEmail().endsWith("@gmail.com") && u.getMobileNumber() > 9000000000L)
+			{
+				u.display();
+			}
+		}
+	}
+}
